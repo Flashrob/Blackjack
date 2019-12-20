16e0d9d0f5ca17512cbb366b0efc8e9f8ad12a27
Robert Kolsek <kolsek.income@gmail.com>
2019-12-20 17:07:45 +0800

    Basic Ai done.

--- script.js
+++ script.js
@@ -35,15 +35,15 @@ const startGame = function () {
     blackjack()
 }
 
+//stand button click function
 const stand = function () {
     const stand = document.querySelector("#stand")
     stand.addEventListener("click", function () {
-        if (game.gameOver === false) {
-            if (game.playerTurn === true) {
-                game.playerTurn = !game.playerTurn
-                ai()
-            } else {
-                ai()
+        //on click, if not game over, set player turn to false, start dealer AI
+        if (!game.gameOver) {
+            if (game.playerTurn) {
+                game.playerTurn = false
+                dealerAi()
             }
         }
 
@@ -54,27 +54,25 @@ const hitCard = function () {
     const hit = document.querySelector("#hit")
     hit.addEventListener("click", function () {
         //if it is playerOne's turn, get another card and remove card from deck
-        if (game.gameOver === false) {
-            if (game.playerTurn === true) {
+        if (!game.gameOver) {
+            if (game.playerTurn) {
                 game.players[0].cards.push(deck[0])
                 game.players[0].totalValue = game.players[0].totalValue + deck[0].weight
                 deck.shift()
+                //display the card
                 display()
+                //check for blackjack
                 blackjack()
-            } else {
-                ai()
+                //if no blackjack, no game over, check for a loss
+                if (!game.gameOver) {
+                    checkForLoss()
+                }
             }
-
         }
         //check for gameOver after click, if not, check for loss.
         if (game.gameOver) {
 
-        } else {
-            if (checkForLoss()) {
-                game.gameOver = true
-            }
         }
-
     })
 }
 
@@ -89,42 +87,74 @@ const blackjack = function () {
 }
 
 const checkForLoss = function () {
+    //if value over 21, lose and game over
     if (game.players[0].totalValue > 21) {
+        game.gameOver = true
+        alert("player loses")
         return true
-    }
-    if (game.players[1].totalValue > 21) {
+    } else if (game.players[game.players.length-1].totalValue > 21) {
+        game.gameOver = true
+        alert("house loses!")
         return true
     }
 }
 
 const determineWinner = function () {
-    
-}
-
-const ai = function () {
-    if (game.playerTurn === false) {
-        if (game.players[1].totalValue > 17) {
-            game.playerTurn = !game.playerTurn
-        } else {
-            game.players[1].cards.push(deck[0])
-            game.players[1].totalValue = game.players[1].totalValue + deck[0].weight
-            deck.shift()
-            game.playerTurn = !game.playerTurn
-            display()
-            blackjack()
-            checkForLoss()
-        }
+    //if player has higher value than dealer, player wins, game over
+    if (game.players[0].totalValue > game.players[game.players.length-1].totalValue) {
+        game.gameOver = true
+        alert("player wins!")
+        return true
+        //if player has lower value than dealer, dealer wins, game over
+    } else if (game.players[0].totalValue < game.players[game.players.length-1].totalValue) {
+        game.gameOver = true
+        alert("house wins!")
+        return true
+        //if player and dealer have the same value, push(draw) and return the bets.
+    } else if (game.players[0].totalValue === game.players[game.players.length-1].totalValue){
+        game.gameOver = true
+        alert("Push! Bets returned!")
+        return true
     }
 }
 
+const dealerAi = function () {
+    if (!game.gameOver) {
+        //house at least 17, determine winner
+        if (game.players[game.players.length-1].totalValue >= 17) {
+            determineWinner()
+            //house lower than 17 and lower than player one, push new card into deck(hit)
+        } else if (game.players[game.players.length-1].totalValue < 17) {
+            game.players[game.players.length-1].cards.push(deck[0])
+            game.players[game.players.length-1].totalValue = game.players[game.players.length-1].totalValue + deck[0].weight
+            deck.shift()
+            //display new score
+            display()
+            //check for blackjack
+            blackjack()
+            //if not blackjack, no game over, check for loss
+            if (!game.gameOver) {
+                checkForLoss()
+            }
+        }
+        //run dealer Ai again, until game is over
+        dealerAi()
+    }
+}
+
+//display the score
 const display = function () {
+    //select text div and reset content
     const text = document.querySelector(".text")
     text.innerHTML = ""
+    //loop through all players
     for (let i = 0; i < game.players.length; i++) {
-
+        //create a div and p for each player
         const div = document.createElement("div")
         const p = document.createElement("p")
+        //set content to player cards value
         p.textContent = `${game.players[i].name}: Value ${game.players[i].totalValue}`
+        //append divs to text div, and paragraphs to divs
         text.appendChild(div)
         div.appendChild(p)
     }
