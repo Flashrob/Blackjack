1b126126c271f71ca2c1b9bc68ca80432a681346
Robert Kolsek <kolsek.income@gmail.com>
2019-12-27 22:23:07 +0800

    Added background image. Adjusted display functions to house and players. Preparation for multiple AI-players are laid. Basic CSS for player and house done.

--- setup.js
+++ setup.js
@@ -22,7 +22,7 @@ const createDeck = function () {
                 suit: suits[j],
                 value: values[i],
                 weight: weight,
-                image: `images/${suits[j][0]}${values[i]}`
+                image: `images/${suits[j][0]}${values[i]}.png`
             }
             
             //push card object to deck
@@ -59,20 +59,59 @@ const dealCards = function () {
     }
 }
 
+//display the cards
+const displayCards = function () {
+    
+    const cards = document.querySelectorAll(".cards")
+    console.log(cards)
+    const houseCards = document.querySelector(".house-cards")
+    cards.forEach(function(item){
+        item.innerHTML = ""
+    })
+    houseCards.innerHTML = ""
+    //display cards for house
+    for (let k = 0; k < game.players[game.players.length-1].cards.length; k++){
+        const img = document.createElement("img")
+            img.setAttribute("src", `${game.players[game.players.length-1].cards[k].image}`)
+            houseCards.appendChild(img)
+    }
+    //display cards for all players
+    for (let i = 0; i < game.players.length - 1; i++){
+        for (let j = 0; j < game.players[i].cards.length; j++){
+            const img = document.createElement("img")
+            img.setAttribute("src", `${game.players[i].cards[j].image}`)
+            cards[i].appendChild(img)
+        }
+    }
+}
+
 //display the score
 const display = function () {
     //select text div and reset content
     const text = document.querySelector(".text")
     text.innerHTML = ""
     //loop through all players
-    for (let i = 0; i < game.players.length; i++) {
+    for (let i = 0; i < game.players.length - 1; i++) {
         //create a div and p for each player
         const div = document.createElement("div")
         const p = document.createElement("p")
         //set content to player cards value
-        p.textContent = `${game.players[i].name}: Value ${game.players[i].totalValue} and Cards: ${game.players[i].cards[0].value} and ${game.players[i].cards[1].value}`
+        p.textContent = `${game.players[i].name}: Value ${game.players[i].totalValue}`
+
         //append divs to text div, and paragraphs to divs
         text.appendChild(div)
         div.appendChild(p)
     }
+    const textHouse = document.querySelector(".house-text")
+
+    textHouse.innerHTML = ""
+    const divHouse = document.createElement("div")
+    const pHouse = document.createElement("p")
+    pHouse.textContent = `${game.players[game.players.length-1].name}: Value ${game.players[game.players.length-1].totalValue}`
+
+    textHouse.appendChild(divHouse)
+    divHouse.appendChild(pHouse)
+
+
+    displayCards()
 }
